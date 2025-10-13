1e9ebc0551e8f5d1094dd2f46109aed0e2ca07c4
KubrakovDmitry <kubrakoff.dmitry@yandex.ru>
2025-10-13 18:37:21 +0300

    feat added views and widget for training model

--- medical-ner/main.py
+++ medical-ner/main.py
@@ -1,17 +1,32 @@
 """FastAPI - приложение для NER."""
 
+import json
+
 from fastapi import FastAPI, Body
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 from ner_model import NERModel
+from ner_trainer import NERTrainer
 
 
 TEXT = "text"
+ANNOTATION = 'dataset'
+MODEL_NAME = 'name_model'
+EPOCHS = 'epochs'
+TRAIN_BATCH_SIZE = 'train_batch_size'
+EVAL_BATCH_SIZE = 'eval_batch_size'
+LEARNING_RATE = 'learning_rate'
+DECAY = 'decay'
+LOGGING_STEP = 'log'
+LR_SCHEDULER_TYPE = 'lr_type'
+WARMUP_RATIO = 'warmup_radio'
+FP16 = 'fp16'
+GRADIENT_ACCUMULATION_STEPS = 'grad_accum'
 
 app = FastAPI()
 model = NERModel()
-
+trainer = NERTrainer()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -33,9 +48,29 @@ def ner(data=Body()):
 
     return {TEXT: text[:text.rfind(';\n')] + '.'}
 
+
 @app.get('/model')
 def get_model_page():
     """Переход на страницу обучения модели."""
-    return FileResponse('model_page.html')
+    return FileResponse('train_page.html')
 
 
+@app.post('/train-ner-model')
+def train(data=Body()):
+    """Обучение модели для NER."""
+
+    trainer.train(
+        annotation=None,
+        model_name=data[MODEL_NAME],
+        epochs=data[EPOCHS],
+        train_batch_size=data[TRAIN_BATCH_SIZE],
+        eval_batch_size=data[EVAL_BATCH_SIZE],
+        learning_rate=data[LEARNING_RATE],
+        decay=data[DECAY],
+        logging_step=data[LOGGING_STEP],
+        lr_scheduler_type=data[LR_SCHEDULER_TYPE],
+        warmup_ratio=data[WARMUP_RATIO],
+        fp16=data[FP16],
+        gradient_accumulation_steps=data[GRADIENT_ACCUMULATION_STEPS]
+    )
+    return {'message': 'Обучение модели NER завершено успешно!'}
